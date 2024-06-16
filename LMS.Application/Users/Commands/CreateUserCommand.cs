235d688e9edc83868e7ea6322fa766960374e7f3
AHMED <ahmed.barq03@gmail.com>
2024-06-16 23:37:39 +0200

    update

--- LMS.Application/Users/Commands/CreateUserCommand.cs
+++ LMS.Application/Users/Commands/CreateUserCommand.cs
@@ -6,6 +6,7 @@ public class CreateUserCommand : IRequest<UserResponse>
 {
     public CreateUserCommand() =>
 
+        // Generate a random 4 digit number for the LibraryCardNumber (deomnstration purposes only)
         LibraryCardNumber = Guid.NewGuid().ToString().Substring(0, 4);
     public int UserId { get; set; }
     public string Name { get; set; }
