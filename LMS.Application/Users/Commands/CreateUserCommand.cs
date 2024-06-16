777b2cd695565ac3a8de7b9dbfe609f5ae03ffd4
AHMED <ahmed.barq03@gmail.com>
2024-06-16 22:59:29 +0200

    create user command

---
+++ LMS.Application/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,10 @@
+namespace LMS.Application.Users.Commands
+{
+    public class CreateUserCommand
+    {
+        public string Name { get; set; }
+        public string ContactInformation { get; set; }
+        public string LibraryCardNumber { get; set; }
+
+    }
+}
