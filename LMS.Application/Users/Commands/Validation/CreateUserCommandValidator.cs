ff6756664a0fe8360393907427c6f0e7d0593031
AHMED <ahmed.barq03@gmail.com>
2024-06-16 22:59:03 +0200

    create user command validator

---
+++ LMS.Application/Users/Commands/Validation/CreateUserCommandValidator.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using LMS.Core.Models;
+
+namespace LMS.Application.Users.Commands.Validation
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(50).WithMessage("Name must not exceed 50 characters");
+
+            RuleFor(x => x.ContactInformation)
+                .NotEmpty().WithMessage("Contact information is required")
+                .MaximumLength(50).WithMessage("Contact information must not exceed 50 characters");
+
+            RuleFor(x => x.LibraryCardNumber)
+                .NotEmpty().WithMessage("Library card number is required")
+                .MaximumLength(50).WithMessage("Library card number must not exceed 50 characters");
+        }
+
+    }
+}
