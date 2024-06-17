ce672034a9a1377ec56f65522a6447adb006e550
AHMED <ahmed.barq03@gmail.com>
2024-06-17 12:34:15 +0200

    update

--- LMS.Application/Users/Commands/CreateUserCommand.cs
+++ LMS.Application/Users/Commands/CreateUserCommand.cs
@@ -4,9 +4,10 @@ using Mediator;
 namespace LMS.Application.Users.Commands;
 public class CreateUserCommand : IRequest<UserResponse>
 {
-    // Generate a random 4 digit number for the LibraryCardNumber (deomnstration purposes only)
     public int UserId { get; set; }
     public string Name { get; set; }
     public string ContactInformation { get; set; }
+
+    // Generate a random 4 char for the LibraryCardNumber (deomnstration purposes only)
     public readonly string LibraryCardNumber = Guid.NewGuid().ToString().Substring(0, 4);
 }
