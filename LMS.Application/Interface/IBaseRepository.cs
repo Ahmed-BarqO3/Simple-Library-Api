500be0764e8d86a12eaa6b2a06e81af1473d6911
AHMED <ahmed.barq03@gmail.com>
2024-06-17 23:53:16 +0200

    update code

--- LMS.Application/Interface/IBaseRepository.cs
+++ LMS.Application/Interface/IBaseRepository.cs
@@ -4,11 +4,11 @@ using Microsoft.Data.SqlClient;
 namespace LMS.Application.Interface;
 public interface IBaseRepository<T> where T : class, new()
 {
-    Task<IEnumerable<T?>> GetAllAsync();
+    Task<IEnumerable<T?>> GetAllAsync(string[] includes = null);
     Task<T?> GetByIdAsync(int id);
     Task AddAsync(T entity);
     void Update(T entity);
     void Delete(T id);
-    Task<T?> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> FindAsync(Expression<Func<T, bool>> predicate,string[] includes = null);
     IQueryable<T?> ExecuteStoredProc(string commandName);
 }
