a9c74fc15c5bdf42e65b629659de9bf909fc12a6
AHMED <ahmed.barq03@gmail.com>
2024-06-17 10:30:14 +0200

    add user command handler

---
+++ LMS.Application/Users/Commands/Handlers/CreateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.Application.Interface;
+using LMS.Application.Response;
+using LMS.Core.Models;
+using Mapster;
+using Mediator;
+
+namespace LMS.Application.Users.Commands.Handlers;
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserResponse>
+{
+    private readonly IUnitOfWork _context;
+
+    public CreateUserCommandHandler(IUnitOfWork context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask<UserResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = request.Adapt<User>();
+        await _context.Users.AddAsync(user);
+
+        _context.Save();
+
+        return user.Adapt<UserResponse>();
+    }
+}
