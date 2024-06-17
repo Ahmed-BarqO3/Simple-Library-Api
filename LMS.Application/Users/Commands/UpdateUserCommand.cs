f34f55347b313d9314c7c19a2766c4bfe7d2cc2e
AHMED <ahmed.barq03@gmail.com>
2024-06-17 12:44:55 +0200

    update

--- LMS.Application/Users/Commands/UpdateUserCommand.cs
+++ LMS.Application/Users/Commands/UpdateUserCommand.cs
@@ -8,5 +8,6 @@ namespace LMS.Application.Users.Commands
         public int UserId { get; set; }
         public string Name { get; set; }
         public string ContactInformation { get; set; }
+        public string LibraryCardNumber { get; set; }
     }
 }
