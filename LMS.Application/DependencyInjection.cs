5480a75665c2a4b228901a1483658ba69d4100e1
AHMED <ahmed.barq03@gmail.com>
2024-06-19 21:38:15 +0200

    inject UriService

--- LMS.Application/DependencyInjection.cs
+++ LMS.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
 ﻿using FluentValidation;
+using LMS.Application.Common;
+using LMS.Application.Interface;
 using Mediator;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LMS.Application;
@@ -14,6 +17,16 @@ public static class DependencyInjection
         });
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PipelineBehavior.ValidationBehavior<,>));
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+        
+        
+        services.AddScoped<IUriService>(provider =>
+        {
+            var accessor = provider.GetRequiredService<IHttpContextAccessor>();
+            var request = accessor.HttpContext.Request;
+            var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+            return new UriService(uri);
+        });
+        
 
         return services;
     }
