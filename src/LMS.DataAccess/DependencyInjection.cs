2b9019ba649bbc6cfdc2f4fe613a80399ce7b49e
AHMED <ahmed.barq03@gmail.com>
2024-06-19 21:40:53 +0200

    refactor

---
+++ src/LMS.DataAccess/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using LMS.Application.Interface;
+using LMS.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddTransient<IUnitOfWork,UnitOfWork>();
+
+        return services;
+    }
+}
